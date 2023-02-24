62efcc18184dbd2d807ee3e79f529aba3a49e643
Sergi-GV <sergio.gzvelez@gmail.com>
2023-02-24 11:06:07 +0100

    Primera versión de la API

---
+++ ApiService v1.0/src/main/java/com/example/appfaltasapiservice/repositorio/AvisoGuardiaRepositorio.java
@@ -0,0 +1,9 @@
+package com.example.appfaltasapiservice.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.appfaltasapiservice.modelo.AvisoGuardia;
+
+public interface AvisoGuardiaRepositorio extends JpaRepository<AvisoGuardia, Integer>{
+	
+}
---
+++ ApiService v1.0/src/main/java/com/example/appfaltasapiservice/repositorio/HorarioCentroRepositorio.java
@@ -0,0 +1,9 @@
+package com.example.appfaltasapiservice.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.appfaltasapiservice.modelo.HorarioCentro;
+
+public interface HorarioCentroRepositorio extends JpaRepository<HorarioCentro, Integer>{
+
+}
